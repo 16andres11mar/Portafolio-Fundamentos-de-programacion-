57ce1a3227fded5d6392a91f9c2cfab5365a8a3f
16andres11mar <99025232+16andres11mar@users.noreply.github.com>
2022-03-04 12:22:27 -0500

    Delete Proyectos directory

--- Proyectos/Operadores basicos/Operador basico (3).py
+++
@@ -1,7 +0,0 @@
-
-
-numero1= 3
-n_umero1= 2
-n2umero= 7
-
-print (numero1 + n_umero1 + n2umero)
--- Proyectos/Variables/variable.py
+++
@@ -1,7 +0,0 @@
-mensaje = "Fundamentos de programacion"
-n = 17
-pi= 3.14159
-
-print (mensaje)
-print (n)
-print (pi)
